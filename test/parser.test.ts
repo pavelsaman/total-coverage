346dd1f76e4a09cc4177544f680e2dab1207ffb3
Pavel Saman <samanpavel@gmail.com>
2023-09-01 22:55:52 +0200

    add more tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,8 +1,20 @@
-// $ lcov --rc branch_coverage=1 --summary fixtures/lcov.info
+// $ lcov --rc branch_coverage=1 --summary test/fixtures/lcov.info
 // Summary coverage rate:
 //   lines......: 60.8% (163 of 268 lines)
 //   functions..: 60.4% (29 of 48 functions)
 //   branches...: 51.1% (166 of 325 branches)
+
+// $ lcov --rc branch_coverage=1 --summary test/fixtures/only-a.info
+// Summary coverage rate:
+//   lines......: 67.5% (104 of 154 lines)
+//   functions..: 68.0% (17 of 25 functions)
+//   branches...: 59.4% (98 of 165 branches)
+
+// $ lcov --rc branch_coverage=1 --summary test/fixtures/only-c.info
+// Summary coverage rate:
+//   lines......: 85.7% (36 of 42 lines)
+//   functions..: 70.0% (7 of 10 functions)
+//   branches...: 59.7% (43 of 72 branches)
 
 import assert from 'node:assert/strict';
 import { test } from 'node:test';
@@ -31,4 +43,52 @@ test('parse() returns correct coverage counters', () => {
   assert.strictEqual(coverage.functions.hit, 29);
   assert.strictEqual(coverage.branches.found, 325);
   assert.strictEqual(coverage.branches.hit, 166);
+});
+
+test(`parse(): "files" should contain correct coverage counters when source files were passed`, () => {
+  const sourceFile = 'apps/api/src/a.ts';
+  const coverage = parse(fixtureLcov, [sourceFile]);
+
+  assert.strictEqual(coverage.lines.found, 268);
+  assert.strictEqual(coverage.lines.hit, 163);
+  assert.strictEqual(coverage.functions.found, 48);
+  assert.strictEqual(coverage.functions.hit, 29);
+  assert.strictEqual(coverage.branches.found, 325);
+  assert.strictEqual(coverage.branches.hit, 166);
+
+  assert('files' in coverage);
+  assert.strictEqual(coverage.files[sourceFile].lines.found, 154);
+  assert.strictEqual(coverage.files[sourceFile].lines.hit, 104);
+  assert.strictEqual(coverage.files[sourceFile].functions.found, 25);
+  assert.strictEqual(coverage.files[sourceFile].functions.hit, 17);
+  assert.strictEqual(coverage.files[sourceFile].branches.found, 165);
+  assert.strictEqual(coverage.files[sourceFile].branches.hit, 98);
+});
+
+test('parse(): calculate total coverage counters and coverage counters for two source files', () => {
+  const sourceFileA = 'apps/api/src/a.ts';
+  const sourceFileC = 'apps/api/src/c.ts';
+  const coverage = parse(fixtureLcov, [sourceFileA, sourceFileC]);
+
+  assert.strictEqual(coverage.lines.found, 268);
+  assert.strictEqual(coverage.lines.hit, 163);
+  assert.strictEqual(coverage.functions.found, 48);
+  assert.strictEqual(coverage.functions.hit, 29);
+  assert.strictEqual(coverage.branches.found, 325);
+  assert.strictEqual(coverage.branches.hit, 166);
+
+  assert('files' in coverage);
+  assert.strictEqual(coverage.files[sourceFileA].lines.found, 154);
+  assert.strictEqual(coverage.files[sourceFileA].lines.hit, 104);
+  assert.strictEqual(coverage.files[sourceFileA].functions.found, 25);
+  assert.strictEqual(coverage.files[sourceFileA].functions.hit, 17);
+  assert.strictEqual(coverage.files[sourceFileA].branches.found, 165);
+  assert.strictEqual(coverage.files[sourceFileA].branches.hit, 98);
+
+  assert.strictEqual(coverage.files[sourceFileC].lines.found, 42);
+  assert.strictEqual(coverage.files[sourceFileC].lines.hit, 36);
+  assert.strictEqual(coverage.files[sourceFileC].functions.found, 10);
+  assert.strictEqual(coverage.files[sourceFileC].functions.hit, 7);
+  assert.strictEqual(coverage.files[sourceFileC].branches.found, 72);
+  assert.strictEqual(coverage.files[sourceFileC].branches.hit, 43);
 });
--- test/totalCoverage.test.ts
+++ test/totalCoverage.test.ts
@@ -1,8 +1,20 @@
-// $ lcov --rc branch_coverage=1 --summary fixtures/lcov.info
+// $ lcov --rc branch_coverage=1 --summary test/fixtures/lcov.info
 // Summary coverage rate:
 //   lines......: 60.8% (163 of 268 lines)
 //   functions..: 60.4% (29 of 48 functions)
 //   branches...: 51.1% (166 of 325 branches)
+
+// $ lcov --rc branch_coverage=1 --summary test/fixtures/only-a.info
+// Summary coverage rate:
+//   lines......: 67.5% (104 of 154 lines)
+//   functions..: 68.0% (17 of 25 functions)
+//   branches...: 59.4% (98 of 165 branches)
+
+// $ lcov --rc branch_coverage=1 --summary test/fixtures/only-c.info
+// Summary coverage rate:
+//   lines......: 85.7% (36 of 42 lines)
+//   functions..: 70.0% (7 of 10 functions)
+//   branches...: 59.7% (43 of 72 branches)
 
 import assert from 'node:assert/strict';
 import { test } from 'node:test';
@@ -27,4 +39,37 @@ test('totalCoverage() returns correct total coverages', () => {
   assert.strictEqual(coverage.totalLineCov, 60.8);
   assert.strictEqual(coverage.totalFunctionCov, 60.4);
   assert.strictEqual(coverage.totalBranchCov, 51.1);
+});
+
+test('totalCoverage(): returns correct total coverages also for passed source files', () => {
+  const sourceFile = 'apps/api/src/a.ts';
+  const coverage = totalCoverage(fixtureLcov, [sourceFile]);
+
+  assert.strictEqual(coverage.totalLineCov, 60.8);
+  assert.strictEqual(coverage.totalFunctionCov, 60.4);
+  assert.strictEqual(coverage.totalBranchCov, 51.1);
+
+  assert('files' in coverage);
+  assert.strictEqual(coverage.files[sourceFile].totalLineCov, 67.5);
+  assert.strictEqual(coverage.files[sourceFile].totalFunctionCov, 68);
+  assert.strictEqual(coverage.files[sourceFile].totalBranchCov, 59.4);
+});
+
+test('totalCoverage(): calculate total coverage and coverage for two source files', () => {
+  const sourceFileA = 'apps/api/src/a.ts';
+  const sourceFileC = 'apps/api/src/c.ts';
+  const coverage = totalCoverage(fixtureLcov, [sourceFileA, sourceFileC]);
+
+  assert.strictEqual(coverage.totalLineCov, 60.8);
+  assert.strictEqual(coverage.totalFunctionCov, 60.4);
+  assert.strictEqual(coverage.totalBranchCov, 51.1);
+
+  assert('files' in coverage);
+  assert.strictEqual(coverage.files[sourceFileA].totalLineCov, 67.5);
+  assert.strictEqual(coverage.files[sourceFileA].totalFunctionCov, 68);
+  assert.strictEqual(coverage.files[sourceFileA].totalBranchCov, 59.4);
+
+  assert.strictEqual(coverage.files[sourceFileC].totalLineCov, 85.7);
+  assert.strictEqual(coverage.files[sourceFileC].totalFunctionCov, 70);
+  assert.strictEqual(coverage.files[sourceFileC].totalBranchCov, 59.7);
 });
